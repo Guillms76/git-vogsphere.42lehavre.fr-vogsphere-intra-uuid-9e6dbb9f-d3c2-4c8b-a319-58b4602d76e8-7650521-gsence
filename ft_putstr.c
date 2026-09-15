25fc64bf89bc826514fc8f9a1c83209ee7ad953f
Guillaume <gsence@orange.fr>
2026-09-15 16:17:57 +0200

    Add files via upload

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,6 +6,26 @@
 /*   By: gsence <gsence@student.42lehavre.fr>      #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/09/10 00:13:12 by gsence           #+#    #+#              */
-/*   Updated: 2026/09/10 00:13:12 by gsence          ###   ########.fr        */
+/*   Updated: 2026/09/14 19:39:20 by gsence          ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	ft_putstr("hello world .. !!!");
+	return (0);
+}
